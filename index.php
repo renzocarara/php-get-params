04152a9bb05964756b56db0b1cf08c2a7387219c
Renzo Carara <renzo.carara@libero.it>
2020-01-20 15:57:37 +0100

    aggiunto  chiamata a qualche altra funzione sulle stringhe

--- index.php
+++ index.php
@@ -11,11 +11,13 @@ echo '<h1>Censuratore</h1>';
 // leggo la bad word dal parametro GET
 $bad_word = $_GET['bad_word'];
 
+// verifico se la badword ha un valore diverso da nullo
 if ($bad_word !="") {
 
     echo '<h3>badword letta: </h3>' . $bad_word;
 
-    $testo="Lorem cuorum dolor sit amet, consectetur adipisicing elit.
+    // testo da censurare
+    $testo = "Lorem cuorum dolor sit amet, consectetur adipisicing elit.
     Voluptates quo minus blanditiis cuorum tenetur tempora delectus culpa,
      error esse quia magnam harum dicta quod quos placeat totam iste.
     Distinctio voluptates perferendis accusamus voluptatum sunt, in, id ex eum rerum quidem,
@@ -25,10 +27,18 @@ if ($bad_word !="") {
     repellendus quos minima, aliquam, aspernatur rerum maiores numquam,
     explicabo delectus! Harum deserunt, unde cupiditat
     neque cuorum omnis, ratione, nam molestias doloremque mollitia cuorum aliquam sed illo?";
+    // stringa per censurare testo
+    $censura="***";
+    // informazioni aggiuntive sul testo
+    $num_parole = str_word_count($testo);
+    $num_caretteri = strlen($testo);
 
-    // echo '<h3> Questo è il testo completo: </h3>' . $testo;
+    echo '<h3> Questo è il testo completo: </h3>' . $testo;
+    echo '<h4>Informazioni aggiuntive:</h4>';
+    echo 'Parole totali: <span style="color:red;">' . $num_parole . '</span><br>';
+    echo 'Caratteri totali: <span style="color:red;">' . $num_caretteri . '</span><br>';
 
-    $testo_censurato = str_replace($bad_word, "***", $testo);
+    $testo_censurato = str_replace($bad_word, $censura, $testo);
     echo '<h3> Questo è il testo censurato: </h3><p>' . $testo_censurato . '</p>';
 
 } else{
